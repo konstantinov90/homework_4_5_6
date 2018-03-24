cc7aa798583f3d3864ee26912dc850dc08e3e0b8
klester <thebraid1@gmail.com>
2018-03-24 16:44:06 +0300

    test heroku

--- backend/GitHandler.js
+++ backend/GitHandler.js
@@ -3,6 +3,7 @@ const path = process.env.ROOT;
 import fs from 'fs';
 import moment from 'moment';
 
+
 import options from '../options';
 moment.locale(options.timeLocale || 'ru');
 import GitCLI from './GitCLI';
